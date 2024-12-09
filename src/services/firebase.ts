08495a4430a54595f909d870fbc005a676f167e9
Alif Rahman <alifrahman2023@gmail.com>
2024-12-09 01:39:57 -0500

    hmm

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,5 @@
 import admin from "firebase-admin";
 
-// path.resolve needed for ensuring that the correct file is loaded
-// no matter where the script is ran from
 const serviceAccount = require("../../serviceAccountKey.json");
 
 // init the app using the key.json from firebase console
